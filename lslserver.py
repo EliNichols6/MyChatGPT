6b504853ec726466912224100e33d6ff82640f40
EliNichols6 <105531174+EliNichols6@users.noreply.github.com>
2023-06-27 21:07:55 -0400

    Update lslserver.py
    Permanent storing of all users' conversations through SQL database

--- lslserver.py
+++ lslserver.py
@@ -1,8 +1,8 @@
 from flask import Flask, request, jsonify
 from dotenv import load_dotenv
-import pickle
-import openai
 import os
+import sqlite3
+import pickle
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import (
     SystemMessage,
@@ -12,74 +12,119 @@ from langchain.schema import (
 
 app = Flask(__name__)
 
-DATA_FILE = 'data.pkl'
+DATA_FILE = '' # Example: data.pkl
+DB_FILE = '' # Example: conversations.db
 
-def save_data(data):
+def save_data(user_messages, user_data):
+    data = {
+        'user_messages': user_messages,
+        'user_data': user_data,
+    }
     with open(DATA_FILE, 'wb') as f:
         pickle.dump(data, f)
 
 def load_data():
     try:
         with open(DATA_FILE, 'rb') as f:
-            return pickle.load(f)
+            data = pickle.load(f)
+            return data['user_messages'], data['user_data']
     except FileNotFoundError:
-        return {}
+        return {}, {}
 
+def setup_db():
+    conn = sqlite3.connect(DB_FILE)
+    c = conn.cursor()
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS conversations (
+            id INTEGER PRIMARY KEY,
+            user_id TEXT,
+            convo_id INTEGER,
+            step_id INTEGER,
+            system_message TEXT,
+            user_message TEXT,
+            ai_response TEXT,
+            timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+        )
+    ''')
+    conn.commit()
+    conn.close()
+
+setup_db()
 
 load_dotenv()  # loads api key from '.env' file
 chat = ChatOpenAI(temperature=0)
 
-# Initialize a dictionary to hold the messages for each user
-user_messages = load_data()
+# Initialize dictionaries to hold the messages for each user and the corresponding data
+user_messages, user_data = load_data()
 
 @app.route('/chat', methods=['POST'])
 def handle_chat():
-    global user_messages
+    global user_messages, user_data
     # Get the message and user_id from the request
     user_input = request.json.get('message')
     user_id = request.json.get('user_id')
 
-    # If this is a new user, initialize their message list
+    # Initialize data for a new user
     if user_id not in user_messages:
         user_messages[user_id] = [SystemMessage(content="You are MyChatGPT, a helpful assistant dedicated to student's learning.")]
-
-    messages = user_messages[user_id]
+        user_data[user_id] = {
+            0: {  # This is the initial conversation
+                'system_message': user_messages[user_id][0],
+                'steps': {}
+            },
+        }
+    
+    current_convo = max(user_data[user_id].keys())  # Gets the latest conversation ID
+    current_step = max(user_data[user_id][current_convo]['steps'].keys()) if user_data[user_id][current_convo]['steps'] else 0  # Gets the latest step ID
 
     if user_input == "clear":
-        messages.clear()
-        messages.append(SystemMessage(content="You are MyChatGPT, a helpful assistant dedicated to student's learning."))
-        user_messages[user_id] = messages
-        save_data(user_messages)
+        # Reset convo_id, step_id, and messages
+        user_messages[user_id] = [SystemMessage(content="You are MyChatGPT, a helpful assistant dedicated to student's learning.")]
+        user_data[user_id][current_convo+1] = {
+            'system_message': user_messages[user_id][0],
+            'steps': {}
+        }
+        save_data(user_messages, user_data)
         return jsonify({"response": "Chat history cleared!"})
 
-    # Append the user message to the messages
-    messages.append(HumanMessage(content=user_input))
+    # Create a new step with the user message
+    user_messages[user_id].append(HumanMessage(content=user_input))
+    user_data[user_id][current_convo]['steps'][current_step+1] = {
+        'user_message': user_messages[user_id][-1],
+        'ai_message': ""
+    }
 
     # Generate a response using ChatGPT
-    ai_response = chat(messages)  
+    ai_response = chat(user_messages[user_id])
+    user_messages[user_id].append(AIMessage(content=ai_response.content))
 
-    # Append the AI message to the messages
-    messages.append(AIMessage(content=ai_response.content))
-    
-    # Explicitly update the message list in the dictionary
-    user_messages[user_id] = messages
-    save_data(user_messages)
+    # Update the AI message for the current step
+    user_data[user_id][current_convo]['steps'][current_step+1]['ai_message'] = user_messages[user_id][-1]
 
-    # Get the assistant's reply
-    assistant_reply = ai_response.content
+    save_data(user_messages, user_data)  # Update pickle data file after every interaction
+
+    # Save to SQLite database
+    conn = sqlite3.connect(DB_FILE)
+    c = conn.cursor()
+    c.execute('''
+        INSERT INTO conversations (
+            user_id,
+            convo_id,
+            step_id,
+            system_message,
+            user_message,
+            ai_response
+        ) VALUES (?, ?, ?, ?, ?, ?)
+    ''', (user_id, current_convo, current_step+1, user_data[user_id][current_convo]['system_message'].content, user_input, ai_response.content))  
+    conn.commit()
+    conn.close()
 
     # Remove newline characters
-    assistant_reply = assistant_reply.replace('\n', ' ')
-
-    # Count the number of user messages
-    user_message_count = sum(isinstance(msg, HumanMessage) for msg in messages)
-
-
-    print(user_messages[user_id])
+    ai_response = ai_response.content.replace('\n', ' ')
 
     # Send the reply back to the requester
-    return jsonify({"response": assistant_reply, "user_message_count": user_message_count})
-   
+    return jsonify({"response": ai_response})
+
 @app.route('/check', methods=['GET'])
 def check_user():
     # Get the user_id from the request
@@ -104,7 +149,5 @@ def check_user():
         "messages": message_contents
     })
 
-
 if __name__ == "__main__":
-    print(user_messages)
     app.run(host='0.0.0.0', port=0)
